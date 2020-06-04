f0bbfbd6702564c8d3dc880a926c0be5026d9f02
Kaspars Zuks <kaspars.zuks@gmail.com>
2020-06-04 17:12:04 +0300

    Description of config files for default template

--- init/default/config/path-config.js
+++ init/default/config/path-config.js
@@ -1,6 +1,8 @@
 /*
  * Path configuration
  * All options will be merged with defaults, but not replaces whole configuration object
+ *
+ * Default configuration can be seen here https://github.com/videinfra/static-website-builder/blob/master/config/path-config.js
  */
 
 // exports.src = './src';
--- init/default/config/task-config.js
+++ init/default/config/task-config.js
@@ -1,16 +1,20 @@
 /*
  * Task configuration
  * All options will be merged with defaults, but not replaces whole configuration object
+ *
+ * Default configuration can be seen here https://github.com/videinfra/static-website-builder/tree/master/tasks
+ * in each tasks config.js file
  */
 
-// exports.clean = {};
-// exports.static = {};
-// exports.html = {};
-// exports.fonts = {};
-// exports.icons = {};
-// exports.images = {};
-// exports.javascripts = {};
-// exports.stylesheets = {};
-// exports.browserSync = {};
-// exports.sizereport = {};
-// exports.plugins = {};
+exports.clean = {};
+exports.static = {};
+exports.html = {};
+exports.fonts = {};
+exports.icons = {};
+exports.images = {};
+exports.javascripts = {};
+exports.stylesheets = {};
+exports.browserSync = {};
+exports.sizereport = {};
+
+exports.plugins = {};
