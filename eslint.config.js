bc97152d50c500fdd01078870b1eeaf7aff14f87
Kaspars Zuks <kaspars.zuks@videinfra.com>
2026-01-28 18:13:45 +0200

    ESLint file change

---
+++ eslint.config.js
@@ -0,0 +1,39 @@
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+	{
+        env: {
+            "browser": true,
+            "commonjs": true,
+            "es6": true,
+            "jquery": true
+        },
+        extends: "eslint:recommended",
+        parserOptions: {
+            "sourceType": "module"
+        },
+        rules: {
+            "linebreak-style": [
+                "error",
+                "unix"
+            ],
+            "quotes": [
+                "error",
+                "single"
+            ],
+            "semi": [
+                "error",
+                "always"
+            ],
+            "no-console": [
+                "warn", { "allow": ["warn", "error"] }
+            ],
+            "no-debugger": [
+                "warn"
+            ],
+            "no-unused-vars": [
+                "warn"
+            ]
+        }
+	},
+]);
